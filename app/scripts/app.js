66fd40620119e5c8f7beb42262bc95bef503a9b4
Wilco Fiers <wilco@pangeadc.nl>
2014-05-21 13:43:24 +0200

    Initial yeoman install

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+  .module('wcagReporterApp', [
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
