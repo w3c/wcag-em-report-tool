a4efe3797d8430ea24e618039c5780d072e08a30
Wilco Fiers <wilco@pangeadc.nl>
2014-06-30 17:13:31 +0200

    Basic sample features added

--- app/scripts/controllers/audit/sample.js
+++ app/scripts/controllers/audit/sample.js
@@ -7,4 +7,18 @@ angular.module('wcagReporter')
     $scope.structuredSample = evalSampleModel.structuredSample;
     $scope.randomSample = evalSampleModel.randomSample;
 
+    $scope.addPage = evalSampleModel.addPage;
+    $scope.removePage = evalSampleModel.removePage;
+
+    $scope.processInput = function () {
+        var errors = evalSampleModel.validate();
+        if (errors.length > 0) {
+            // display errors
+            // prevent default
+        } else {
+            // continue to next step
+        }
+
+        console.log(evalSampleModel);
+    };
 });
--- app/scripts/models/evaluation/sample.js
+++ app/scripts/models/evaluation/sample.js
@@ -2,9 +2,24 @@
 
 angular.module('wcagReporter').service('evalSampleModel', function() {
     
+    function Page() {
+        this.url = 'http://example.com';
+        this.state = 'some state';
+    }
+
     this.structuredSample = [];
 
     this.randomSample = [];
+
+    this.removePage = function (sample, index) {
+        sample.splice(index, 1);
+    };
+
+    this.addPage = function (sample) {
+        console.log(sample);
+        sample.push(new Page());
+    };
+
 
     /**
      * Returns an array of errors indicating which (if any) properties are invalid
