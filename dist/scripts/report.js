46d651642941f9e79f7a818e4b00a9311c31cd22
Wilco Fiers <wilco@pangeadc.nl>
2014-08-11 14:21:42 +0200

    build bug fixed

--- dist/scripts/report.js
+++ dist/scripts/report.js
@@ -1,8 +1,1 @@
-(function ($) {
-	'use strict';
-	$('body').on('click', 'button.toggle-page-details', function () {
-		$('.assert-indent').toggle(300);
-		$('.show_pages').toggle();
-		$('.hide_pages').toggle();
-	});
-}(jQuery));
+!function(a){"use strict";a("body").on("click","button.toggle-page-details",function(){a(".assert-indent").toggle(300),a(".show_pages").toggle(),a(".hide_pages").toggle()})}(jQuery);
