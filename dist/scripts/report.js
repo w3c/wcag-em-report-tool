864c100c8f12243dbc5c0c344a6c98794c8bbb12
Wilco Fiers <wilco@pangeadc.nl>
2014-08-20 13:24:46 +0200

    Example build with macro

--- dist/scripts/report.js
+++ dist/scripts/report.js
@@ -1,1 +1,8 @@
-!function(a){"use strict";a("body").on("click","button.toggle-page-details",function(){a(".assert-indent").toggle(300),a(".show_pages").toggle(),a(".hide_pages").toggle()})}(jQuery);
+(function ($) {
+	'use strict';
+	$('body').on('click', 'button.toggle-page-details', function () {
+		$('.assert-indent').toggle(300);
+		$('.show_pages').toggle();
+		$('.hide_pages').toggle();
+	});
+}(jQuery));
