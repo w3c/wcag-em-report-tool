408f935619bac86a1f6d606bad1021290f5ad3ba
Wilco Fiers <wilco@pangeadc.nl>
2014-08-25 18:05:24 +0200

    Disabled features not part of 1.0 release

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -71,9 +71,9 @@ angular.module('wcagReporter', [
 
 // Setup automatic import/export based on attributes of the root element
 }).run(function (wcagReporterImport, wcagReporterExport, $rootElement) {
-    var autosave = $rootElement.attr('autosave');
+    //var autosave = $rootElement.attr('autosave');
     wcagReporterExport.storage.init({
-        autosave: (autosave === '' || autosave.toLowerCase() === 'autosave'),
+        //autosave: (autosave === '' || autosave.toLowerCase() === 'autosave'),
         url: $rootElement.attr('url'),
         saveDelay: ($rootElement.attr('save-delay') || 1500)
     });
