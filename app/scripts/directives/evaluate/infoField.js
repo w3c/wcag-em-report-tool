c45ab899e3d2e9c5e7f4a9ce6e9950ca46b7de70
Wilco Fiers <wilco@pangeadc.nl>
2014-08-27 10:37:25 +0200

    infoField has a default focus when closing #100

--- app/scripts/directives/evaluate/infoField.js
+++ app/scripts/directives/evaluate/infoField.js
@@ -29,7 +29,21 @@ angular.module('wcagReporter')
     });
 
 
-}).directive('infoField', function(directivePlugin) {
+}).directive('infoField', function(directivePlugin, $document) {
+    /**
+     * Get the next visible element that can receive focus outside the .alert-info
+     */
+    function getNextFocusElement(elm) {
+        var selector = ':input:visible, a[href]:visible',
+            focusable = $document.find(selector);
+
+        elm = angular.element(elm)
+        .closest('.alert-info')
+        .find(selector).last();
+
+        return focusable[focusable.index(elm)+1];
+    }
+
     return directivePlugin({
         restrict: 'E',
         scope: {
@@ -40,19 +54,27 @@ angular.module('wcagReporter')
         link: function (scope, elm) {
             elm.hide(0);
 
-            function hide (e) {
-                // The div doesn't get a 'click' so 
-                // we check for a return keyup
-                if (e.type === 'keyup' &&
-                (e.keyCode !== 13 && e.keyCode !== 27)) {
+            scope.close = function ($event) {
+                if ($event.type === 'keyup' &&
+                ($event.keyCode !== 13 && $event.keyCode !== 27)) {
                     return;
                 }
-                elm.hide(200, function () {
-                    angular.element('#' + scope.exitto).focus();
-                });
-            }
-            elm.on('keyup', hide)
-            .find('button').on('click', hide);
+                getNextFocusElement($event.target).focus();
+                elm.hide(200);
+            };
+            // function hide (e) {
+            //     // The div doesn't get a 'click' so 
+            //     // we check for a return keyup
+            //     if (e.type === 'keyup' &&
+            //     (e.keyCode !== 13 && e.keyCode !== 27)) {
+            //         return;
+            //     }
+            //     elm.hide(200, function () {
+            //         angular.element('#' + scope.exitto).focus();
+            //     });
+            // }
+            // elm.on('keyup', hide)
+            // .find('button').on('click', hide);
         },
         replace: true,
         transclude: true,
