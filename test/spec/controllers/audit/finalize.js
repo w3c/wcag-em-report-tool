d287b60e52d1c1e7b66feb35a7ae35e8841657f5
Wilco Fiers <wilco@pangeadc.nl>
2014-08-28 11:00:21 +0200

    cleaned up tests

--- test/spec/controllers/audit/finalize.js
+++ test/spec/controllers/audit/finalize.js
@@ -16,7 +16,7 @@ describe('Controller: AuditFinalizeCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(scope.awesomeThings.length).toBe(3);
+  // });
 });
