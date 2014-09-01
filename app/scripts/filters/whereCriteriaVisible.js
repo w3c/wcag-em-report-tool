1e3891fe8bf1e260e7ebf470eeb285a7a04871b9
Wilco Fiers <wilco@pangeadc.nl>
2014-09-01 12:30:52 +0200

    Finished whereCriteriaVisible filter

--- app/scripts/filters/whereCriteriaVisible.js
+++ app/scripts/filters/whereCriteriaVisible.js
@@ -1,24 +1,32 @@
 'use strict'; 
 
-angular.module('wcagReporter') 
+/**
+ * For an array of principles or guidelines, remove those where from which the
+ * test functions returns a falsy value
+ */
+angular.module('wcagReporter')
 .filter('whereCriteriaVisible', function() {
 
+    // Filter the principle based on if it has criteria the test passes
     function testPrinciple(principles, test) {
-        return principles.reduce(function (val, principle) {
-            return val || testGuideline(principle.guidelines, test);
+        return principles.filter(function (p) {
+            return testGuideline(p.guidelines, test).length !== 0;
         });
     }
 
+    // Filter the guideline based on if it has criteria the test passes
     function testGuideline(guidelines, test) {
-        return guidelines.reduce(function (val, guideline) {
-            return val || testCriteria(guideline.criteria, test);
+        return guidelines.filter(function (g) {
+            var res = testCriteria(g.criteria, test);
+            return res;
         });
     }
 
+    // For any list of criteria, check if the test says any passes
     function testCriteria(criteria, test) {
-        return criteria.reduce(function (val, criterion) {
-            return val || test(criterion.uri);
-        });
+        return criteria.reduce(function (val, c) {
+            return val || (!!test(c.uri));
+        }, false);
     }
 
     return function(arr, test) {
