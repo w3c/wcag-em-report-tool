8787db5a38770d86b227a0d38e568e86a0a81100
Wilco Fiers <wilco@pangeadc.nl>
2014-09-01 11:52:52 +0200

    whereCritVis has test callback

--- app/scripts/filters/whereCriteriaVisible.js
+++ app/scripts/filters/whereCriteriaVisible.js
@@ -3,8 +3,35 @@
 angular.module('wcagReporter') 
 .filter('whereCriteriaVisible', function() {
 
-    return function(arr) {
-        return arr;
+    function testPrinciple(principles, test) {
+        return principles.reduce(function (val, principle) {
+            return val || testGuideline(principle.guidelines, test);
+        });
+    }
+
+    function testGuideline(guidelines, test) {
+        return guidelines.reduce(function (val, guideline) {
+            return val || testCriteria(guideline.criteria, test);
+        });
+    }
+
+    function testCriteria(criteria, test) {
+        return criteria.reduce(function (val, criterion) {
+            return val || test(criterion.uri);
+        });
+    }
+
+    return function(arr, test) {
+        if (!angular.isArray(arr) || arr.length === 0) {
+            return arr;
+
+        } else if (angular.isDefined(arr[0].guidelines)) {
+            return testPrinciple(arr, test);
+
+        } else if (angular.isDefined(arr[0].criteria)) {
+            return testGuideline(arr, test);
+        }
+
     };
 
 });
