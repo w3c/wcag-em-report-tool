846686f80342df0f0fd6ea9a233c2d7e2e59807e
Wilco Fiers <wilco@>
2015-10-02 18:34:02 +0200

    Fixed a bug of incorrectly opening on the start page

--- app/scripts/controllers/start.js
+++ app/scripts/controllers/start.js
@@ -4,13 +4,16 @@ angular.module('wcagReporter')
 .controller('StartCtrl',
 function ($scope, $location, appState, $timeout, $rootScope) {
   	$scope.state = appState.moveToState('start');
-
-    if (typeof $rootScope.rootHide.start1 !== 'undefined') {
+    
+    if (typeof $rootScope.rootHide.start1 === 'undefined') {
         $scope.initial = 'hidden';
+        console.log($scope.initial);
         $timeout(function () {
+            console.log($scope.initial);
             $scope.initial = '';
         }, 500);
         $timeout(function () {
+            console.log($scope.initial);
             $rootScope.rootHide.start1 = false;
         }, 700);
     }
