7044dfc1d427ede9293dc5c71e5aa4460e74e2d4
Wilco Fiers <wilco@>
2015-11-02 19:05:00 +0100

    start working in NL support

--- Gruntfile.js
+++ Gruntfile.js
@@ -112,7 +112,7 @@ module.exports = function (grunt) {
         },
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
-          '<%= yeoman.app %>/scripts/locales/*.js',
+          '<%= yeoman.app %>/locale/*.json',
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
