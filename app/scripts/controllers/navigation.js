9fcbce7272845211f8f496653b4e6f1b0f09063f
Wilco Fiers <wilco@>
2015-11-28 14:44:47 +0100

    Navigate ctrl now uses wcag20spec loader

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -2,22 +2,11 @@
 
 angular.module('wcagReporter')
 .controller('NavigationCtrl',
-function ($scope, $rootScope, $translate, wcag20spec, localeLoader) {
+function ($scope, $translate, wcag20spec) {
 
   	$scope.changeLanguage = function (lang) {
-
-        localeLoader.loadLang(lang, function (err) {
-            if (!err) {
-                $scope.$apply(function () {
-                    $translate.use(lang);
-                    wcag20spec.useLanguage(lang);
-                    $rootScope.$broadcast('langChange', lang);
-                });
-            } else {
-                console.log(err);
-            }
-        });
-
+        $translate.use(lang);
+        wcag20spec.loadLanguage(lang);
   	};
 
 });
