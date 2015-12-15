9adff53cbe494af4d41b83881374d92a463344b2
Wilco Fiers <wilco@>
2015-12-15 18:07:15 +0100

    Update report on lang change

--- app/scripts/controllers/report/score.js
+++ app/scripts/controllers/report/score.js
@@ -15,46 +15,56 @@ angular.module('wcagReporter')
         'level_aaa': {pass: 0, total: 0}
     };
     $scope.totals = totals;
-    $scope.scores = wcag20spec.getPrinciples().map(function (p) {
-        var result = {
-            name:  p.num + '. ' + p.title,
-            'earl:passed': 0,
-            'earl:failed': 0,
-            'earl:inapplicable': 0,
-            'earl:untested': 0,
-            'earl:cantTell': 0,
-            'tested': 0,
-            'level_a': {pass: 0, total: 0},
-            'level_aa': {pass: 0, total: 0},
-            'level_aaa': {pass: 0, total: 0}
-        };
 
-        // Get all criteria of this principle:
-        p.guidelines.reduce(function (list, guide) {
-            list.push.apply(list, guide.successcriteria);
-            return list;
+    $scope.getScores = function () {
+        return wcag20spec.getPrinciples().map(function (p) {
+            var result = {
+                name:  p.num + '. ' + p.handle,
+                'earl:passed': 0,
+                'earl:failed': 0,
+                'earl:inapplicable': 0,
+                'earl:untested': 0,
+                'earl:cantTell': 0,
+                'tested': 0,
+                'level_a': {pass: 0, total: 0},
+                'level_aa': {pass: 0, total: 0},
+                'level_aaa': {pass: 0, total: 0}
+            };
 
-        }, []).forEach(function (crit) {
-        // For each, set the result
-            var critResult = evalAuditModel.getCritAssert(crit.id);
-            if (critResult) {
-                var outcome = critResult.result.outcome;
-                var level = crit.level.replace('wcag20:level_', '');
+            // Get all criteria of this principle:
+            p.guidelines.reduce(function (list, guide) {
+                list.push.apply(list, guide.successcriteria);
+                return list;
 
-                result[outcome] += 1;
-                $scope.totals[outcome] += 1;
-                if (outcome === 'earl:passed' ||
-                    outcome === 'earl:inapplicable') {
-                    result['level_' + level].pass += 1; 
-                    totals['level_' + level].pass += 1;
+            }, []).forEach(function (crit) {
+            // For each, set the result
+                var critResult = evalAuditModel.getCritAssert(crit.id);
+                if (critResult) {
+                    var outcome = critResult.result.outcome;
+                    var level = crit.level.replace('wcag20:level_', '');
+
+                    result[outcome] += 1;
+                    $scope.totals[outcome] += 1;
+                    if (outcome === 'earl:passed' ||
+                        outcome === 'earl:inapplicable') {
+                        result['level_' + level].pass += 1; 
+                        totals['level_' + level].pass += 1;
+                    }
+                    if (outcome !== 'earl:untested') {
+                        result['tested'] += 1;
+                        result['level_' + level].total += 1; 
+                        totals['level_' + level].total += 1;
+                    }
                 }
-                if (outcome !== 'earl:untested') {
-                    result['tested'] += 1;
-                    result['level_' + level].total += 1; 
-                    totals['level_' + level].total += 1;
-                }
-            }
+            });
+            return result;
         });
-        return result;
+    };
+
+    $scope.scores = $scope.getScores(); 
+    // Update the score name when the language changes
+    $scope.$on('wcag20spec:langChange', function () {
+        $scope.scores = $scope.getScores(); 
     });
+    
 });
