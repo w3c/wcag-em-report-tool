9a4ada0aa0f9d25234c5c26403d32143dbcc2df4
Wilco Fiers <wilco@>
2015-12-28 15:50:22 +0100

    Warn users when opening with a popup blocker

--- app/scripts/services/evalLoader.js
+++ app/scripts/services/evalLoader.js
@@ -3,45 +3,51 @@
 angular.module('wcagReporter')
 .service('evalLoader', function (evalWindow, appState, fileReader, wcagReporterImport, $q) {
 
-	function loadFactory(promiseGen) {
+    function loadFactory(promiseGen) {
         var importTarget;
 
         return function () {
-            var promise = promiseGen.apply(null, arguments),
-            	defer = $q.defer();
+            var promise = promiseGen.apply(null, arguments);
+            var defer = $q.defer();
             
             function reject(e) {
-            	defer.reject(e);
-            	importTarget.abort();
+                defer.reject(e);
+                if (importTarget) {
+                   importTarget.abort();
+                }
             }
 
             if (!appState.empty) {
-                importTarget = evalWindow.openEmptyWindow();
+                try {
+                    importTarget = evalWindow.openEmptyWindow();
+                } catch (e) {
+                    reject('Popup blocker detected. Please allow popups so the evaluation can open in a new window.');
+                }
             }  else {
                 importTarget = evalWindow;
             }
 
             promise.then(function (data) {
-            	try {
-                	importTarget.loadJson(data);
-                	appState.setDirtyState();
-                	defer.resolve();
-            	} catch (e) {
-            		reject(e);
-            	}
+                try {
+                    importTarget.loadJson(data);
+                    appState.setDirtyState();
+                    defer.resolve();
+                } catch (e) {
+                    reject(e);
+                }
             }, reject);
 
             return defer.promise;
         };
     }
 
-	return {
-		openFromFile: loadFactory(function (file) {
-	        return fileReader.readAsText(file);
-	    }),
+    return {
+        openFromFile: loadFactory(function (file) {
+            return fileReader.readAsText(file);
+        }),
 
-	    openFromUrl: loadFactory(function () {
+        openFromUrl: loadFactory(function () {
             return wcagReporterImport.getFromUrl();
-	    })
-	};
+        })
+    };
 });
