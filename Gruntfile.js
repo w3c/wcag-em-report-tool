982cfc8c2b72cdd1a881e564f3ffe86e7e4eef1d
Roel Antonisse <roel.antonisse@gmail.com>
2019-07-18 14:53:07 +0200

    Replace grunt-ngmin with grunt-ng-annotate
    (Deprecated)

--- Gruntfile.js
+++ Gruntfile.js
@@ -304,10 +304,10 @@ module.exports = function (grunt) {
       }
     },
 
-    // ngmin tries to make the code safe for minification automatically by
+    // ngAnnotate tries to make the code safe for minification automatically by
     // using the Angular long form for dependency injection. It doesn't work on
     // things like resolve or inject so those have to be done manually.
-    ngmin: {
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -458,7 +458,7 @@ module.exports = function (grunt) {
     'useminPrepare',
     'concurrent:dist',
     'concat',
-    'ngmin',
+    'ngAnnotate',
     'copy:dist',
     'cssmin',
     'htmlmin:distView',
