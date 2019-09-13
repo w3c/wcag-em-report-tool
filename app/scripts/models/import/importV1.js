f7f2e27b2c59fa7a5b0c2de6b949eedb3ecd1908
Roel Antonisse <roel.antonisse@gmail.com>
2019-09-13 15:51:54 +0200

    Add readability to version 1 import conversion

--- app/scripts/models/import/importV1.js
+++ app/scripts/models/import/importV1.js
@@ -1,23 +1,32 @@
 'use strict';
 
-angular.module('wcagReporter')
-  .factory('importV1', function (evalContextV1, evalContextV2, $filter) {
+angular
+  .module('wcagReporter')
+  .factory('importV1', function (
+    evalContextV1,
+    evalContextV2,
+    $filter
+  ) {
     var getUrl = $filter('getUrl');
 
     function convertor (importArray) {
-      return importArray.map(function (importObj) {
-        // upgrade from v1 to v2
-        if (isV1Evaluation(importObj)) {
-          importObj = upgradeToV2(importObj);
+      return importArray
+        .map(function (importObj) {
+          // upgrade from v1 to v2
+          if (isV1Evaluation(importObj)) {
+            importObj = upgradeToV2(importObj);
 
           // Correct the foaf namespace
-        } else if (typeof importObj === 'object' &&
-                typeof importObj['@context'] === 'object' &&
-                importObj['@context']['@vocab'] === 'http://xmlns.com/foaf/spec/#') {
-          importObj['@context']['@vocab'] = 'http://xmlns.com/foaf/0.1/';
-        }
-        return importObj;
-      });
+          } else if (
+            typeof importObj === 'object' &&
+            typeof importObj['@context'] === 'object' &&
+            importObj['@context']['@vocab'] === 'http://xmlns.com/foaf/spec/#'
+          ) {
+            importObj['@context']['@vocab'] = 'http://xmlns.com/foaf/0.1/';
+          }
+
+          return importObj;
+        });
     }
 
     /**
@@ -27,53 +36,67 @@ angular.module('wcagReporter')
       if (typeof data !== 'object') {
         throw new TypeError('Expected object for ' + data);
       }
+
       var dataContext = data['@context'];
       var contextProps = Object.keys(evalContextV1);
+
       // Skip if the context isn't there
       if (typeof dataContext !== 'object') {
         return false;
       }
 
       // Dirty check if they have the same keys
-      if (contextProps.sort()
-        .join(',') !== Object.keys(dataContext)
-        .sort()
-        .join(',')) {
+      if (
+        (
+          contextProps
+            .sort()
+            .join(',')
+        ) !== (
+          Object.keys(dataContext)
+            .sort()
+            .join(',')
+        )
+      ) {
         return false;
       }
 
-      return contextProps.reduce(function (result, prop) {
-        if (!result) { // false is false
-          return result;
+      return contextProps
+        .reduce(function (result, prop) {
+          if (!result) { // false is false
+            return result;
 
           // Context prop doesn't exist
-        } else if (typeof dataContext[prop] === 'undefined') {
-          return false;
+          } else if (typeof dataContext[prop] === 'undefined') {
+            return false;
 
           // Context prop is different value
-        } else if (typeof dataContext[prop] === 'string' &&
-                dataContext[prop] !== evalContextV1[prop]) {
-          return false;
+          } else if (
+            typeof dataContext[prop] === 'string' &&
+            dataContext[prop] !== evalContextV1[prop]
+          ) {
+            return false;
 
           // Context prop is an object, compare it's content
-        } else if (typeof dataContext[prop] === 'object') {
-          return Object.keys(evalContextV1[prop])
-            .reduce(function (result, subProp) {
-              if (!result) {
-                return result;
-              } else if (typeof dataContext[prop][subProp] === 'undefined') {
-                return false;
-              } else if (typeof dataContext[prop][subProp] === 'string' &&
-                        dataContext[prop][subProp] !== evalContextV1[prop][subProp]) {
-                return false;
-              } else {
-                return true;
-              }
-            }, true);
-        } else {
-          return true;
-        }
-      }, true);
+          } else if (typeof dataContext[prop] === 'object') {
+            return Object.keys(evalContextV1[prop])
+              .reduce(function (result, subProp) {
+                if (!result) {
+                  return result;
+                } else if (typeof dataContext[prop][subProp] === 'undefined') {
+                  return false;
+                } else if (
+                  typeof dataContext[prop][subProp] === 'string' &&
+                  dataContext[prop][subProp] !== evalContextV1[prop][subProp]
+                ) {
+                  return false;
+                } else {
+                  return true;
+                }
+              }, true);
+          } else {
+            return true;
+          }
+        }, true);
     }
 
     /** Upgrade Page to v2 */
@@ -84,8 +107,10 @@ angular.module('wcagReporter')
           'WebPage'
         ];
       }
+
       page.title = page.handle;
       delete page.handle;
+
       var source = getUrl(page.description);
       if (source) {
         page.source = source;
@@ -114,13 +139,14 @@ angular.module('wcagReporter')
         tech.type = tech.type || 'Technology';
       });
 
-      var evalScope = evaluation.evaluationScope;
       if (evalScope.conformanceTarget.substr(0, 13) === 'wcag20:level_') {
         evalScope.conformanceTarget = 'wai:WCAG2' + (
-          evalScope.conformanceTarget.replace('wcag20:level_', '')
+          evalScope.conformanceTarget
+            .replace('wcag20:level_', '')
             .toUpperCase()
         ) + '-Conformance';
       }
+
       if (evalScope.website.title) {
         evalScope.website.siteName = evalScope.website.title;
         delete evalScope.website.title;
