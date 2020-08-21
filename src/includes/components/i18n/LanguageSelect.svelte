5b5a122fbcac6c13f3dfad0ac0925916bd270f78
Roel Antonisse <r.antonisse@accessibility.nl>
2020-08-21 17:14:42 +0200

    Add a clickHandle on LanguageSelect and change to target language
    (svelte-i18n locale.set)

--- src/includes/components/i18n/LanguageSelect.svelte
+++ src/includes/components/i18n/LanguageSelect.svelte
@@ -1,5 +1,5 @@
 <!-- @Component:LanguageSelect -->
-<ul id="languageSelect" title="languages">
+<ul id="languageSelect" title="languages" on:click={handleClick}>
   {#each locales as locale}
     <li><a
       href="/{locale.lang}/"
@@ -11,7 +11,25 @@
 <!-- /@Component -->
 
 <script>
+  import { locale } from 'svelte-i18n';
   import appData from '../../../data/app.js';
 
   let { locales, defaultLocale } = appData;
+
+  /**
+   * Handle the languageSelect click,
+   * Changing to target language
+   * @param  {[object]} event [description]
+   * @return {undefined}
+   */
+  function handleClick(event) {
+    const target = event.target;
+
+    if (
+      target.nodeName === 'A' &&
+      locales.some(l => l.lang === target.lang)
+    ) {
+      locale.set(target.lang);
+    }
+  }
 </script>
