b1ca31c4e5c50453e2e63c4e9cba88cf20219a8e
Roel Antonisse <r.antonisse@accessibility.nl>
2020-08-21 15:03:52 +0200

    Apply i18n to startpage /

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -1,39 +1,3 @@
-<script context="module">
-  /**
-   * Using this script to import content data and push forward to component
-   */
-
-  import i18n from '../../data/i18n/index.js';
-  import appData from '../../data/app.js';
-
-  export async function preload(page, session) {
-
-    const {
-      lang
-    } = page.params;
-    const {
-      locales,
-      defaultLocale
-    } = appData;
-    const isLocale = locales.some(oLocale => oLocale.lang === lang);
-
-    if (isLocale) {
-
-      let pageData = true ?
-        i18n[lang].pages.home :
-        i18n[defaultLocale].pages.home;
-      pageData.translated = true;
-
-      pageData.locale = locales
-        .find(oLocale => oLocale.lang === lang) || defaultLocale;
-
-      return {
-        page: pageData
-      };
-    }
-  }
-</script>
-
 <!--
  * @Page:/[lang]/
  * This is a "dynamic page" if you have a link or path like this:
@@ -41,51 +5,51 @@
  * page.params will have a variable called locale with value 'en'
  * We can use this to set other vars or get data based on the locale.
  * -->
-<Page {title}>
-  {#each sections as section, index }
-    <details open={index === 0}>
-      <summary><h2>{ section.title }</h2></summary>
-      {#if section.tag === "p"}
-        {#each section.contents as content}
-          <p>{@html content}</p>
-        {/each}
-      {:else if section.tag === "ul"}
-        <ul>
-        {#each section.contents as content}
-          <li>{@html content}</li>
-        {/each}
-        </ul>
-      {/if}
+<Page title={ $translate('start.SUBTITLE') }>
 
-    </details>
-  {/each}
+  <details open="true">
+    <summary><h2>{ $translate('start.INTRO_HD') }</h2></summary>
+    <p>{ $translate('start.INTRO_1', { values: links}) }</p>
+    <p>{ $translate('start.INTRO_2') }</p>
+  </details>
+
+  <details>
+    <summary><h2>{ $translate('start.USAGE_HD') }</h2></summary>
+    <ul>
+      <li>{ $translate('start.USAGE_LI1') }</li>
+      <li>{ $translate('start.USAGE_LI2') }</li>
+      <li>{ $translate('start.USAGE_LI3') }</li>
+    </ul>
+  </details>
+
+  <details>
+    <summary><h2>{ $translate('start.TIPS_HD') }</h2></summary>
+    <ol>
+      <li>{ $translate('start.TIPS_LI1') }</li>
+      <li>{ $translate('start.TIPS_LI2') }</li>
+      <li>{ $translate('start.TIPS_LI3') }</li>
+      <li>{ $translate('start.TIPS_LI4') }</li>
+    </ol>
+  </details>
+
 </Page>
 <!-- /@Page -->
 
 <script>
+  import { stores } from '@sapper/app';
+  import { t as translate, locale } from 'svelte-i18n';
+
   import Page from '../../includes/components/Page.svelte';
-  export let page;
 
-  $: title = page.title;
-  $: locale = page.locale;
-  $: nextPage = {
-    href: `/${page.locale.lang}/evaluation/scope/`
+  const { page } = stores();
+
+  console.dir($page)
+
+  // $: lang = page.params.lang;
+  $: links = {
+    link_wcag: `<a href="#wcagem">Web Content Accessibility Guidelines Evaluation Methodology</a>`,
+    link_wcagem: `<a href="#wcagem">Web Content Accessibility Guidelines Evaluation Methodology</a>`,
+    link_wcagem_overview: `<a href="#wcagem">Web Content Accessibility Guidelines Evaluation Methodology</a>`,
+    link_report_example: `<a href="#wcagem">Web Content Accessibility Guidelines Evaluation Methodology</a>`
   };
-  $: translated = page.translated;
-  $: sections = [
-    {
-      tag: 'p',
-      ...page.description
-    },
-    {
-      tag: 'ul',
-      ...page.usage
-    },
-    {
-      tag: 'ul',
-      ...page.tips
-    }
-  ];
-
-  // @TODO: Find a way to extend / set the title prop from layout
 </script>
