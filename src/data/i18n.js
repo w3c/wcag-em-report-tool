027b67afe691195e268f3e823a0f0b92c484b50b
Roel Antonisse <r.antonisse@accessibility.nl>
2020-08-21 17:27:46 +0200

    Add nl (Dutch) translations to app

--- src/data/i18n.js
+++ src/data/i18n.js
@@ -1,10 +1,17 @@
-import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
+import { register, init, getLocaleFromPathname } from 'svelte-i18n';
+
+import app from './app.js';
 
 // Asynchronous loading
 register('en', () => import('./i18n/en.js'));
+register('nl', () => import('./i18n/nl.js'));
 
 init({
-  fallbackLocale: 'en',
-  initialLocale: getLocaleFromNavigator(),
+  fallbackLocale: app.defaultLocale.lang,
+  // language[-region]
+  // e.g. /nl-NL/ --> Dutch Netherlands
+  // See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes for language codes
+  // See https://en.wikipedia.org/wiki/ISO_3166-1 for region codes
+  initialLocale: getLocaleFromPathname(/^\/([a-z]{2})(\-[A-Z]{2})?\//),
 });
 // starts loading 'en'
