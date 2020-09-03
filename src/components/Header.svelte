0e9bbb4086c9096f23883fc0a408eb68e341dae4
Roel Antonisse <r.antonisse@accessibility.nl>
2020-09-03 12:50:11 +0200

    Add styling to Header, add utility classes for visibility

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,28 @@
 <!-- @component:Header -->
-<header id="site-header">
-  <div class="brand-name"><p>WCAG-EM Report Tool</p></div>
+<header class="tool-header">
+  <div class="tool-header-name">WCAG-EM R<span class="display-phablet">eport </span>T<span class="display-phablet">ool</span></div>
   <Branding />
 </header>
 <!-- /@component -->
+
+<style>
+  header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  header > * {
+    flex-shrink: 0;
+    flex-grow: 1;
+  }
+
+  .tool-header {
+    color: white;
+    background-color: var(--w3c-classic);
+  }
+</style>
 
 <script>
   import Branding from './Branding.svelte';
