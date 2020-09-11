1f715f63655aa6e60ba5859f6a924515681988d6
Roel Antonisse <r.antonisse@accessibility.nl>
2020-09-11 13:12:49 +0200

    Change footer location to index html
    (Add WAI site footer)

--- src/components/layouts/BaseLayout.svelte
+++ src/components/layouts/BaseLayout.svelte
@@ -3,12 +3,9 @@
 <Menubar />
 
 <slot />
-
-<Footer />
 <!-- /@Layout -->
 
 <script>
   import Header from '../Header.svelte';
   import Menubar from '../Menubar.svelte';
-  import Footer from '../Footer.svelte';
 </script>
