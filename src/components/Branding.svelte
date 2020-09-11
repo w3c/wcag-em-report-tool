2a0de99783fb14fee817faa2dde0f66f5bacc33f
Roel Antonisse <r.antonisse@accessibility.nl>
2020-09-11 22:57:37 +0200

    Fix image paths

--- src/components/Branding.svelte
+++ src/components/Branding.svelte
@@ -2,13 +2,13 @@
 <div class="logos">
   <a href="http://w3.org/"><img
       alt="W3C"
-      src="/images/w3c.svg"
+      src="images/w3c.svg"
       width="73"
       height="50"
     /></a>
   <a href="http://w3.org/WAI/"><img
       alt="Web Accessibility Initiative"
-      src="/images/wai.svg"
+      src="images/wai.svg"
       width="120"
       height="50"
     /></a>
