dd2edf2b3455c22d2f3ca4ac2a0da47fa59d4d60
Roel Antonisse <r.antonisse@accessibility.nl>
2020-09-11 22:38:00 +0200

    Update LanguageSelect; Add initial translations, pick styling from WAI

--- src/components/i18n/LanguageSelect.svelte
+++ src/components/i18n/LanguageSelect.svelte
@@ -1,29 +1,52 @@
 <!-- @Component:LanguageSelect -->
-<span>Language ({currentLocale.title})</span>
-<ul id="languageSelect" title="languages" on:click="{handleClick}">
-  {#each locales as appLocale}
-    <li>
-      <button
-        type="button"
-        class="button button-{appLocale === currentLocale ? 'secondary' : 'primary'}"
-        lang="{appLocale.lang}"
-      >{appLocale.title}
-        {#if appLocale.lang === defaultLocale.lang}(Default){/if}</button>
-    </li>
-  {/each}
-</ul>
+<div class="LanguageSelect">
+  <button
+    type="button"
+    class="LanguageSelect__toggle showhidebutton button-small"
+  >{$translate('UI.LANGUAGESELECTOR_BUTTON_NAME', { default: 'Select language' })}
+    (<span
+      class="visually-hidden"
+    >{$translate('UI.LANGUAGESELECTOR_CURRENT_LANGUAGE', {
+        default: 'Current language'
+      })}:
+    </span>{currentLocale.title})</button>
+  <ul id="languages--list" title="languages" on:click="{handleClick}">
+    {#each locales as appLocale}
+      <li
+        class="language__item {appLocale === currentLocale ? 'language__item--current' : ''}"
+      >
+        <a
+          lang="{appLocale.lang}"
+          href="#language-{appLocale.lang}"
+        >{appLocale.title}
+          {#if appLocale.lang === defaultLocale.lang}(Default){/if}</a>
+      </li>
+    {/each}
+  </ul>
+</div>
 <!-- /@Component -->
 
 <style>
-  ul {
+  #languages--list {
+    display: inline-flex;
+    flex-direction: row;
     margin: 0;
     padding: 0;
     list-style-type: none;
+    font-size: 0.75em;
+  }
+
+  .language__item {
+    margin: 0 0.25em;
+  }
+
+  .language__item--current > a {
+    text-decoration: none;
   }
 </style>
 
 <script>
-  import { locale } from 'svelte-i18n';
+  import { t as translate, locale } from 'svelte-i18n';
   import appData from '../../data/app.js';
 
   let { locales, defaultLocale } = appData;
@@ -40,9 +63,11 @@
     const target = event.target;
 
     if (
-      target.nodeName === 'BUTTON' &&
+      target.nodeName === 'A' &&
       locales.some((l) => l.lang === target.lang)
     ) {
+      event.preventDefault();
+
       // This line is what makes the App translate to another language
       locale.set(target.lang);
     }
