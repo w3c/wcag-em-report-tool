55f3a55b8127cfbbc476330cac47cab7fcdf8d80
Roel Antonisse <r.antonisse@accessibility.nl>
2020-09-17 11:26:39 +0200

    Update sample page; Add initial contents

--- src/components/pages/Evaluation/SamplePage.svelte
+++ src/components/pages/Evaluation/SamplePage.svelte
@@ -1,5 +1,42 @@
-<Page title="Sample" />
+<Page title="{$translate('SAMPLE.TITLE')}">
+  <p>{$translate('SAMPLE.INTRO')}</p>
+  {$translate('SAMPLE.HD_STRUCT_SAMPLE')}
+
+<form action="" novalidate>
+  <Textarea
+    id="essential_functionality"
+    label="{$translate('SAMPLE.HD_ESSENT_FUNC')}"
+  />
+
+  <Textarea
+    id="page_types"
+    label="{$translate('SAMPLE.HD_VARIETY_PAGE_TYPES')}"
+  />
+
+  <!-- listinput -->
+  <Textarea
+    label="{$translate('SAMPLE.HD_STRUCT_SAMPLE_SUB')}"
+    helptext="{$translate('SAMPLE.INF_STRUCT_SAMPLE')}"
+  />
+
+  <!-- listinput -->
+  <Textarea
+    label="{$translate('SAMPLE.HD_RANDOM_SAMPLE')}"
+    helptext="{$translate('SAMPLE.INF_RAND_SAMPLE')}"
+  />
+  <p>{$translate('SAMPLE.RAND_SAMPLE_LENGTH')}</p>
+</form>
+</Page>
 
 <script>
+  import { t as translate } from 'svelte-i18n';
+
+  /**
+   * Use evaluation context data here:
+   * - data from ExplorePage
+   */
+
   import Page from '../../Page.svelte';
+
+  import Textarea from '../../formcomponents/Textarea.svelte';
 </script>
