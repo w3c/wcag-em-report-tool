15e394347985d2f133aa2f37a957d0a0652abcf3
Roel Antonisse <r.antonisse@accessibility.nl>
2020-09-23 10:54:50 +0200

    Update AuditPage; Add Panel with auditing tools

--- src/components/pages/Evaluation/AuditPage.svelte
+++ src/components/pages/Evaluation/AuditPage.svelte
@@ -1,5 +1,16 @@
 <Page title="Audit" />
 
+<Panel title="Auditor menu">
+  <p>Some auditing tools...</p>
+  <p>Filter by:</p>
+  <ul>
+    <li>WCAG version</li>
+    <li>Conformance level</li>
+  </ul>
+  <button type="button" class="button-secondary">Import findings</button>
+</Panel>
+
 <script>
   import Page from '../../Page.svelte';
+  import Panel from '../../Panel.svelte';
 </script>
