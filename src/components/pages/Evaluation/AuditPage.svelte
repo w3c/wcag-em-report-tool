113c14aa808d5a3506be25bd0a349a2ab868ca70
Roel Antonisse <r.antonisse@accessibility.nl>
2020-09-24 20:49:48 +0200

    Add Auditor to AuditPage

--- src/components/pages/Evaluation/AuditPage.svelte
+++ src/components/pages/Evaluation/AuditPage.svelte
@@ -6,68 +6,9 @@
     'AUDIT.SAMPLE_FINDINGS' 'AUDIT.BTN_EXPAND_PAGES' 'AUDIT.BTN_COLLAPSE_PAGES'
   </div>
 
-  <div>
-    {'spec.num'}
-    {'spec.handle'}
-    {'spec.text'}
-    {'item.handle'}
-    {'item.text'}
-    {'item.handle'}
-    {'item.text'}
-    {'details.text'}
-    {'spec.number'}
-    {'spec.number'}
-  </div>
-
   <p class="info">Auditor</p>
 
-  <div>
-    <ul>
-      <li>
-        <div>
-          <header>
-            <h3><span>test.number</span> test.title</h3>
-            <span>(test.conformanceLevel)</span>
-          </header>
-
-          <div>
-            <details>
-              <summary>
-                <h4>Show test.title details</h4>
-              </summary>
-              <div>test.description.introduction</div>
-              <span>test.description.details</span>
-              <dl>
-                <dt>detail.title</dt>
-                <dd>
-                  <p>detail.description</p>
-                  <p class="note">detail.note</p>
-                </dd>
-              </dl>
-              <div>
-                <span>test.description.notes</span>
-                <ol>
-                  <li>
-                    <p class="note"><span>Note:</span> <span>note</span></p>
-                  </li>
-                </ol>
-              </div>
-            </details>
-          </div>
-        </div>
-
-        <div>
-          <span>subject.type?</span>: <span>subject.title</span> - (<span
-          >subject.url</span>)
-        </div>
-
-        <div>
-          <div>result.outcome</div>
-          <div>result.description</div>
-        </div>
-      </li>
-    </ul>
-  </div>
+  <Auditor />
 </Page>
 
 <Panel title="Auditor menu">
@@ -85,4 +26,5 @@
 
   import Page from '../../Page.svelte';
   import Panel from '../../Panel.svelte';
+  import Auditor from '../../Auditor.svelte';
 </script>
