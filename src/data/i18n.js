16b14eb9b4889246ec610be2a4d24bf83fea8de1
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-01 20:29:50 +0200

    Cleanup i18n data

--- src/data/i18n.js
+++ src/data/i18n.js
@@ -1,4 +1,4 @@
-import { register, init, getLocaleFromPathname } from 'svelte-i18n';
+import { register, init } from 'svelte-i18n';
 
 import app from './app.js';
 
@@ -12,6 +12,8 @@ init({
   // e.g. /nl-NL/ --> Dutch Netherlands
   // See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes for language codes
   // See https://en.wikipedia.org/wiki/ISO_3166-1 for region codes
-  initialLocale: getLocaleFromPathname(/^\/([a-z]{2})(\-[A-Z]{2})?\//)
+  // initialLocale: getLocaleFromPathname(/^\/([a-z]{2})(\-[A-Z]{2})?\//)
+  initialLocale: 'en'
+
 });
 // starts loading 'en'
