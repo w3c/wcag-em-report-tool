59053ab273ec8aea3b99c31b2d0801ec2a78dfe8
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-08 15:41:25 +0200

    Update SummaryPage; Wrap formelements in a form

--- src/components/pages/Evaluation/SummaryPage.svelte
+++ src/components/pages/Evaluation/SummaryPage.svelte
@@ -2,41 +2,43 @@
 
   <p>{@html $translate('PAGES.REPORT.INTRO')}</p>
 
-  <Input
-    id="evaluation_title"
-    label="{$translate('PAGES.REPORT.LABEL_TITLE')}"
-    helptext="{$translate('PAGES.REPORT.INF_REPORT_TITLE')}"
-  />
+  <form action="" novalidate>
+    <Input
+      id="evaluation_title"
+      label="{$translate('PAGES.REPORT.LABEL_TITLE')}"
+      helptext="{$translate('PAGES.REPORT.INF_REPORT_TITLE')}"
+    />
 
-  <Input
-    id="evaluation_commissioner"
-    label="{$translate('PAGES.REPORT.LABEL_COMMISSIONER')}"
-    helptext="{$translate('PAGES.REPORT.INF_COMMISSIONER')}"
-  />
+    <Input
+      id="evaluation_commissioner"
+      label="{$translate('PAGES.REPORT.LABEL_COMMISSIONER')}"
+      helptext="{$translate('PAGES.REPORT.INF_COMMISSIONER')}"
+    />
 
-  <Input
-    id="evaluation_creator"
-    label="{$translate('PAGES.REPORT.LABEL_CREATOR')}"
-    helptext="{$translate('PAGES.REPORT.INF_CREATOR')}"
-  />
+    <Input
+      id="evaluation_creator"
+      label="{$translate('PAGES.REPORT.LABEL_CREATOR')}"
+      helptext="{$translate('PAGES.REPORT.INF_CREATOR')}"
+    />
 
-  <Input
-    id="evaluation_date"
-    label="{$translate('PAGES.REPORT.LABEL_DATE')}"
-    helptext="{$translate('PAGES.REPORT.INF_DATE')}"
-  />
+    <Input
+      id="evaluation_date"
+      label="{$translate('PAGES.REPORT.LABEL_DATE')}"
+      helptext="{$translate('PAGES.REPORT.INF_DATE')}"
+    />
 
-  <Textarea
-    id="evaluation_summary"
-    label="{$translate('PAGES.REPORT.LABEL_SUMMARY')}"
-    helptext="{$translate('PAGES.REPORT.INF_SUMMARY')}"
-  />
+    <Textarea
+      id="evaluation_summary"
+      label="{$translate('PAGES.REPORT.LABEL_SUMMARY')}"
+      helptext="{$translate('PAGES.REPORT.INF_SUMMARY')}"
+    />
 
-  <Textarea
-    id="evaluation_specifics"
-    label="{$translate('PAGES.REPORT.LABEL_SPECIFICS')}"
-    helptext="{$translate('PAGES.REPORT.INF_SPECIFICS')}"
-  />
+    <Textarea
+      id="evaluation_specifics"
+      label="{$translate('PAGES.REPORT.LABEL_SPECIFICS')}"
+      helptext="{$translate('PAGES.REPORT.INF_SPECIFICS')}"
+    />
+  </form>
 
   <h2>{$translate('PAGES.REPORT.HD_CRITERIA_REPORT')}</h2>
   <!-- Criteria here... -->
