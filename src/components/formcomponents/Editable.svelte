190d5bea5af8b550fd5dfc0790821a703382c2c9
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-08 14:25:47 +0200

    Add initial idea for Editable component

---
+++ src/components/formcomponents/Editable.svelte
@@ -0,0 +1,43 @@
+<div class="Editable">
+  <div class="Editable__Contents" tabindex="0" title="editable text">
+    <slot />
+  </div>
+
+  <div class="Editable__Controls">
+    {#if editMode}
+      <button
+        type="button"
+        class="Editable__Control--edit"
+        on:click={handleUpdateClick}
+      >Done</button>
+    {:else}
+      <button
+        type="button"
+        class="Editable__Control--edit"
+        on:click={toggleEditMode}
+      >Edit</button>
+    {/if}
+    <button
+      type="button"
+      class="Editable__Control--delete button-secondary"
+      on:click={dispatchDelete}
+    >Delete</button>
+  </div>
+</div>
+
+<script>
+  let editMode = false;
+
+  function handleUpdateClick() {
+    console.log('UPDATE');
+    toggleEditMode();
+  }
+
+  function dispatchDelete() {
+    console.log('DELETE');
+  }
+
+  function toggleEditMode() {
+    editMode = !editMode;
+  }
+</script>
