25690949ffa5f46eff41b98b658f1eacf13c1fb2
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-08 14:55:09 +0200

    Add a simple Button wrapper component

---
+++ src/components/Button.svelte
@@ -0,0 +1,9 @@
+<button type="button" class="Button Button--{type} {buttonClass}" on:click>
+  <slot />
+</button>
+
+<script>
+  export let type = 'primary';
+
+  let buttonClass = `button-${type}`;
+</script>
