35816950c4d8c08b5435836f8be45f049788cc02
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-15 16:33:09 +0200

    Update Input; Add autofocus that focuses after Mount

--- src/components/formcomponents/Input.svelte
+++ src/components/formcomponents/Input.svelte
@@ -1,11 +1,21 @@
 <Field id="{id}" label="{label}">
-  <input id="{id}" type="text" name="" bind:value />
+  <input id="{id}" type="text" name="" bind:value bind:this={textInput} />
 </Field>
 
 <script>
+  import { onMount } from 'svelte';
   import Field from './Field.svelte';
 
   export let id;
   export let label;
   export let value = '';
+  export let autofocus = false;
+
+  let textInput;
+
+  onMount(() => {
+    if (autofocus) {
+      textInput.focus();
+    }
+  });
 </script>
