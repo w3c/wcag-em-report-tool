fb612ed6aea25cfc9a186037431d6fad83d069f4
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-20 14:29:57 +0200

    Add basic Grid component

---
+++ src/components/Grid.svelte
@@ -0,0 +1,19 @@
+<div class="{classNames}">
+  <slot />
+</div>
+
+<style>
+  .Grid {
+    display: grid;
+  }
+</style>
+
+<script>
+  export let rightside = false;
+
+  $: classNames = [
+    'Grid',
+    'default-grid',
+    rightside ? 'Grid__rightside' : '',
+  ].join(' ');
+</script>
