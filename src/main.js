0f2dd6d8df804f8eb197949a20fc46e9b5059cbf
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-22 17:56:31 +0200

    Update index / main;
    - Add WAI stylesheet corrections and app namespace

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './data/i18n.js';
  * https://svelte.dev/docs#Creating_a_component
  */
 new App({
-  target: document.getElementById('wcag-rt'),
+  target: document.getElementById('wai-wcag-em-report-tool'),
   props: {
     // Replaced string, see rollup.config.json replace plugin.
     basepath: '__BASEPATH__'
