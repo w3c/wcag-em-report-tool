3124be7cbb2bd79ee2c8161513a4aa8910f1d8e5
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-22 15:11:46 +0200

    Update ReportPage; Add download box with buttons

--- src/components/pages/Evaluation/ReportPage.svelte
+++ src/components/pages/Evaluation/ReportPage.svelte
@@ -1,5 +1,17 @@
 <Page title="{$translate('PAGES.REPORT.TITLE')}">
-  <p>{@html $translate('PAGES.REPORT.INTRO')}</p>
+  <p>
+    {@html $translate('PAGES.REPORT.INTRO')}
+  </p>
+
+  <aside class="box">
+    <header class="box-h">
+      <h2>{$translate('PAGES.REPORT.DOWNLOAD_REPORT')}</h2>
+    </header>
+    <div class="box-i">
+      <button><span>{$translate('PAGES.REPORT.BTN_SAVE_HTML')}</span></button>
+      <button><span>{$translate('PAGES.REPORT.BTN_SAVE_JSON')}</span></button>
+    </div>
+  </aside>
 
   <Report />
 </Page>
