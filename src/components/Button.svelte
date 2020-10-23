305e158fc400d0380b7e8cfb0d5ac437ee7f5a3a
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-23 12:03:03 +0200

    Update Button; Add fake and small boolean properties

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,9 +1,29 @@
-<button type="button" class="Button Button--{type} {buttonClass}" on:click>
-  <slot />
-</button>
+{#if fake}
+  <span class="{classNames}"><slot /></span>
+{:else}
+  <button type="button" class="{classNames}" on:click>
+    <slot />
+  </button>
+{/if}
+
+<style>
+  .Button {
+    box-sizing: border-box;
+    min-width: 1.5rem;
+  }
+</style>
 
 <script>
   export let type = 'primary';
+  export let fake = false;
+  export let small = false;
 
-  let buttonClass = `button-${type}`;
+  let classNames = [
+    'Button',
+    `Button--${type}`,
+    small ? 'Button--small' : '',
+    'button',
+    `button-${type}`,
+    small ? 'button-small' : ''
+  ].join(' ');
 </script>
