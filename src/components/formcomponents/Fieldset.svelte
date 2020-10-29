af5b44cd9797c9709c63d9224f8b2e0c1693951d
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-29 15:25:09 +0100

    Add a Fieldset component

---
+++ src/components/formcomponents/Fieldset.svelte
@@ -0,0 +1,38 @@
+<fieldset id="{id}" class="Fieldset field">
+  <legend>{@html legend}</legend>
+
+  {#if helptext}
+    <div class="Fieldset__helptext">
+      <Details label="Show info">
+        {@html helptext}
+      </Details>
+    </div>
+  {/if}
+
+  <div class="Fieldset__elements">
+    <slot />
+  </div>
+</fieldset>
+
+<style>
+  .Fieldset {
+    padding: 0;
+  }
+
+  .Fieldset__helptext {
+    margin: 1em 0;
+  }
+
+  :global(.Fieldset__elements > *:not(:last-child)) {
+    margin-bottom: 2rem;
+  }
+</style>
+
+<script>
+  import Details from '../Details.svelte';
+
+  export let id;
+  export let legend;
+  export let helptext;
+
+</script>
