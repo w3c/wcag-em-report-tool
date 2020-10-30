77d9e01cb47824bbd1f9d3dd7b52e33ddefb77b8
Roel Antonisse <r.antonisse@accessibility.nl>
2020-10-30 17:12:07 +0100

    Add initial AuditorSamples for sample selection to evaluate

---
+++ src/components/Auditor/AuditorSamples.svelte
@@ -0,0 +1,43 @@
+<Panel title="{$translate('PAGES.AUDIT.HD_SAMPLE_SELECT')}" open>
+  <Details label="show info">
+    <div>{$translate('PAGES.AUDIT.INF_AUDIT_SAMPLE')}</div>
+  </Details>
+  <div>{$translate('PAGES.AUDIT.TESTED')}</div>
+
+  <div class="buttongroup actionbar">
+    <div class="button">
+      <input id="AuditorSamples__multiselect" type="checkbox" indeterminate="true" /><label for="AuditorSamples__multiselect" class="visuallyhidden">{$translate('PAGES.AUDIT.SELECT_ALL')}</label>
+    </div>
+    <button class="button">V<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_COMPLETE_SELECTED')}</span></button>
+    <button class="button">X<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_UNCOMPLETE_SELECTED')}</span></button>
+    <button class="button">^<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_OPEN_SELECTED')}</span></button>
+  </div>
+
+  {#each samples as sample}
+    <div>
+      <input id="{sample.id}" type="checkbox" />
+      <label for="{sample.id}">{sample.title}</label>
+      <button type="button">Open</button>
+    </div>
+  {:else}
+    <div>{$translate('PAGES.AUDIT.NO_SAMPLE')}</div>
+  {/each}
+  <div class=""></div>
+</Panel>
+
+<script>
+  import { getContext } from 'svelte';
+  import { t as translate } from 'svelte-i18n';
+
+  import Details from '../Details.svelte';
+  import Panel from '../Panel.svelte';
+
+  const { sampleStore } = getContext('app');
+
+  $: samples = [
+    ...$sampleStore['STRUCTURED_SAMPLE'],
+    ...$sampleStore['RANDOM_SAMPLE']
+  ];
+
+  console.log(samples);
+</script>
