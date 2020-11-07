9f7a4bcbdb62d297a312f40a8e0bb3dfde81ee90
Roel Antonisse <r.antonisse@accessibility.nl>
2020-11-07 16:07:53 +0100

    Add artoo wcag scrape utility to docs

---
+++ docs/translations/scrape_wcag_spec.js
@@ -0,0 +1,189 @@
+// https://medialab.github.io/artoo/
+
+/**
+ * WCAG2.0
+ */
+
+// SC DATA WCAG20
+WCAG20 = artoo.scrape('.sc', {
+  num: function getSectionNumber() {
+    return $(this)
+      .find('.sc-handle')
+      .text()
+      .match(/\d(\.\d+){2}/g)[0];
+  },
+  conformanceLevel: function getConformanceLevel() {
+    return $(this)
+      .text()
+      .match(/(\(Level (A{1,3})\))/g)
+      .toString()
+      .replace(/[^A]/g, '');
+  }
+});
+artoo.saveJson(WCAG20, 'WCAG20.json');
+
+// SC DATA WCAG21
+WCAG21_EN = artoo.scrape('.sc', {
+  num: function () {
+    return $(this)
+      .find('.secno')
+      .text()
+      .replace(/([^0-9\.])+/g, '')
+      .trim();
+  },
+  conformanceLevel: function () {
+    return $(this)
+      .find('.conformance-level')
+      .text()
+      .replace(/[^A]/g, '')
+      .trim();
+  }
+});
+
+let WCAG21_P = artoo
+  .scrape('.principle', {
+    num: function () {
+      return $(this)
+        .find('h2 > .secno')
+        .text()
+        .replace(/([^0-9])+/g, '')
+        .trim();
+    },
+    ID: 'id',
+    TITLE: function () {
+      return $(this)
+        .find('h2')
+        .text()
+        .replace(/[^A-Za-z\s]+/g, '')
+        .trim();
+    },
+    DESCRIPTION: function () {
+      return $(this)
+        .find('h2 + p')
+        .text()
+        .replace(/\s{2,}/g, ' ')
+        .trim();
+    }
+  })
+  .reduce((translations, item) => {
+    const { num, ...rest } = item;
+
+    translations[num] = rest;
+
+    return translations;
+  }, {});
+
+let WCAG21_G = artoo
+  .scrape('.guideline', {
+    num: function () {
+      return $(this)
+        .find('h3 > .secno')
+        .text()
+        .replace(/([^0-9\.])+/g, '')
+        .trim();
+    },
+    ID: 'id',
+    TITLE: function () {
+      const $el = $(this);
+      const secno = $el.find('h3 > .secno').text();
+
+      return $el
+        .find('h3')
+        .text()
+        .replace(secno, '')
+        .replace(/[^A-Za-z\s]+/g, '')
+        .trim();
+    },
+    DESCRIPTION: function () {
+      return $(this)
+        .find('h3 + p')
+        .text()
+        .replace(/\s{2,}/g, ' ')
+        .trim();
+    }
+  })
+  .reduce((translations, item) => {
+    const { num, ...rest } = item;
+
+    translations[num] = rest;
+
+    return translations;
+  }, {});
+
+let WCAG21_SC = artoo
+  .scrape('.sc', {
+    num: function () {
+      return $(this)
+        .find('.secno')
+        .text()
+        .replace(/([^0-9\.])+/g, '')
+        .trim();
+    },
+    ID: 'id',
+    TITLE: {
+      sel: 'h4',
+      method: function () {
+        const nonTitles = $(this)
+          .find('*')
+          .get()
+          .map((nonTitle) => nonTitle.textContent);
+
+        let title = $(this).text();
+
+        nonTitles.forEach((nonTitle) => {
+          title = title.replace(nonTitle, '');
+        });
+
+        return title.replace(/\s+/g, ' ').trim();
+      }
+    },
+    DESCRIPTION: {
+      sel: '.conformance-level + p',
+      method: function () {
+        return $(this).text().replace(/\s+/g, ' ').trim();
+      }
+    },
+    DETAILS: function () {
+      const list = $(this).find('dl');
+
+      if (!list || list.length === 0) {
+        return {};
+      }
+
+      const titles = list
+        .find('dt')
+        .get()
+        .map((el) => el.textContent.replace(':', '').trim());
+
+      const descriptions = list
+        .find('dd')
+        .get()
+        .map((el) => el.textContent.replace(/\s+/g, ' ').trim());
+
+      return titles.reduce((detailsO, title, index) => {
+        detailsO[`DETAIL_${index}`] = {
+          TITLE: title,
+          DESCRIPTION: descriptions[index]
+        };
+
+        return detailsO;
+      }, {});
+    }
+  })
+  .reduce((translations, item) => {
+    const { num, ...rest } = item;
+
+    translations[num] = rest;
+
+    return translations;
+  }, {});
+
+let WCAG21_TRANSLATION = {
+  PRINCIPLE: WCAG21_P,
+  GUIDELINE: WCAG21_G,
+  SUCCESS_CRITERION: WCAG21_SC
+};
+
+artoo.saveJson(WCAG21_TRANSLATION, 'WCAG21_LANG.json');
+
+// SC CONTENT WCAG21
