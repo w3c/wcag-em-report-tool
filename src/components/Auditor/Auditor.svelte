fa49448121208e19618059b2d8cb19bf3aa6a13c
Roel Antonisse <r.antonisse@accessibility.nl>
2020-11-13 11:51:58 +0100

    Add AuditorFilter and use in Auditor

--- src/components/Auditor/Auditor.svelte
+++ src/components/Auditor/Auditor.svelte
@@ -3,28 +3,7 @@
     <AuditorSamples />
   </div>
 
-  <div class="Auditor__Filter box">
-    <header class="Auditor__Filter__header box-h">
-      {$translate('UI.AUDITOR.FILTER_TITLE', { default: 'Show' })}
-    </header>
-
-    <div class="Auditor__Filters box-i">
-      <MultipleChoice
-        id="filter_wcag_version"
-        label="Criterion WCAG Version"
-        type="radio"
-        options="{['WCAG 2.1', 'Added in WCAG 2.1', 'WCAG 2.0']}"
-        value="WCAG 2.1"
-      />
-
-      <MultipleChoice
-        id="filter_conformance_level"
-        label="Criterion conformance level"
-        options="{['Level A', 'Level AA', 'Level AAA']}"
-        value="{['Level A', 'Level AA']}"
-      />
-    </div>
-  </div>
+  <AuditorFilter />
 
   <div class="Auditor__Assertions">
     <AuditorView criteria="{$wcagStore}" />
@@ -38,17 +17,6 @@
   .Auditor__samples {
     grid-area: left;
     grid-row: 1 / span 2;
-  }
-
-  .Auditor__Filter {
-    grid-area: content;
-    grid-row: 1;
-    margin-bottom: 3rem;
-  }
-
-  .Auditor__Filters {
-    columns: 4 20rem;
-    column-gap: 2rem;
   }
 
   :global(.Auditor .Auditor__Filters > *) {
@@ -88,9 +56,9 @@
   import { getContext } from 'svelte';
   import { t as translate } from 'svelte-i18n';
 
+  import AuditorFilter from './AuditorFilter.svelte';
   import AuditorSamples from './AuditorSamples.svelte';
   import AuditorView from './AuditorView.svelte';
-  import MultipleChoice from '../formcomponents/MultipleChoice.svelte';
 
   const { wcagStore } = getContext('app');
 
---
+++ src/components/Auditor/AuditorFilter.svelte
@@ -0,0 +1,41 @@
+<div class="Auditor__Filter box">
+  <header class="Auditor__Filter__header box-h">
+    {$translate('UI.AUDITOR.FILTER_TITLE', { default: 'Show' })}
+  </header>
+
+  <div class="Auditor__Filters box-i">
+    <MultipleChoice
+      id="filter_wcag_version"
+      label="Criterion WCAG Version"
+      type="radio"
+      options="{['WCAG 2.1', 'Added in WCAG 2.1', 'WCAG 2.0']}"
+      value="WCAG 2.1"
+    />
+
+    <MultipleChoice
+      id="filter_conformance_level"
+      label="Criterion conformance level"
+      options="{['Level A', 'Level AA', 'Level AAA']}"
+      value="{['Level A', 'Level AA']}"
+    />
+  </div>
+</div>
+
+<style>
+  .Auditor__Filter {
+    grid-area: content;
+    grid-row: 1;
+    margin-bottom: 3rem;
+  }
+
+  .Auditor__Filters {
+    columns: 2 20rem;
+    column-gap: 2rem;
+  }
+</style>
+
+<script>
+  import { t as translate } from 'svelte-i18n';
+
+  import MultipleChoice from '../formcomponents/MultipleChoice.svelte';
+</script>
