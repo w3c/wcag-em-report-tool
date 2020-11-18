697e94f62382ee871ad134f214eac0d5fde31baf
Roel Antonisse <r.antonisse@accessibility.nl>
2020-11-18 14:53:52 +0100

    Update sampleStore; Add an allSamples derived store

--- src/data/stores/sampleStore.js
+++ src/data/stores/sampleStore.js
@@ -1,8 +1,12 @@
-import { writable } from 'svelte/store';
+import { derived, writable } from 'svelte/store';
 
 const sampleStore = writable({
   STRUCTURED_SAMPLE: [],
   RANDOM_SAMPLE: []
+});
+
+export const allSamples = derived(sampleStore, ($sampleStore) => {
+  return [...$sampleStore['STRUCTURED_SAMPLE'], ...$sampleStore['RANDOM_SAMPLE']];
 });
 
 export default sampleStore;
