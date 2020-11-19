0c3023025c629fd4b5b247115c2ed8cdceda86d5
Roel Antonisse <r.antonisse@accessibility.nl>
2020-11-19 13:49:22 +0100

    Update AuditorSamples; Use $allSamples store and style updates

--- src/components/Auditor/AuditorSamples.svelte
+++ src/components/Auditor/AuditorSamples.svelte
@@ -3,49 +3,53 @@
     <div>{$translate('PAGES.AUDIT.INF_AUDIT_SAMPLE')}</div>
   </Details>
 
-  <div class="buttongroup actionbar">
-    <input
-      id="AuditorSamples__multiselect"
-      type="checkbox"
-      indeterminate="true"
-    /><label
-      for="AuditorSamples__multiselect"
-      class="visuallyhidden"
-    >{$translate('PAGES.AUDIT.SELECT_ALL')}</label>
-    <button class="button button-small button-secondary">V<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_COMPLETE_SELECTED')}</span></button>
-    <button class="button button-small button-secondary">X<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_UNCOMPLETE_SELECTED')}</span></button>
-    <button class="button button-small button-secondary">^<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_OPEN_SELECTED')}</span></button>
-  </div>
-
-  {#each samples as sample}
+  <Flex align="center" justify="stretch">
     <div>
-      <input id="{sample.id}" type="checkbox" />
-      <label for="{sample.id}">{sample.title}
-        {#if sample.completed}
-          <span class="visuallyhidden">{$translate('PAGES.AUDIT.TESTED')}</span>
-        {/if}
-      </label>
-      <button type="button">Open</button>
+      <input
+        id="AuditorSamples__multiselect"
+        type="checkbox"
+        indeterminate="true"
+      /><label
+        for="AuditorSamples__multiselect"
+        class="visuallyhidden"
+      >{$translate('PAGES.AUDIT.SELECT_ALL')}</label>
     </div>
+    <div style="margin-left: auto;">
+      <button class="button button-small button-secondary">V<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_COMPLETE_SELECTED')}</span></button>
+      <button class="button button-small button-secondary">X<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_UNCOMPLETE_SELECTED')}</span></button>
+      <button class="button button-small button-secondary">^<span class="visuallyhidden">{$translate('PAGES.AUDIT.BTN_OPEN_SELECTED')}</span></button>
+    </div>
+  </Flex>
+
+  <hr />
+
+  {#each $allSamples as sample}
+    <Flex align="center" justify="start">
+      <Flex align="start" justify="start">
+        <input id="{sample.id}" type="checkbox" />
+        <label
+          for="{sample.id}"
+        >{sample.title || sample.description || sample.id}
+          {#if sample.completed}
+            <span
+              class="visuallyhidden"
+            >{$translate('PAGES.AUDIT.TESTED')}</span>
+          {/if}
+        </label>
+      </Flex>
+      <div><button type="button" class="button-small">Open</button></div>
+    </Flex>
   {:else}
     <div>{$translate('PAGES.AUDIT.NO_SAMPLE')}</div>
   {/each}
-  <div class=""></div>
 </Panel>
 
 <script>
-  import { getContext } from 'svelte';
   import { t as translate } from 'svelte-i18n';
 
+  import { allSamples } from '../../data/stores/sampleStore.js';
+
   import Details from '../Details.svelte';
+  import Flex from '../Flex.svelte';
   import Panel from '../Panel.svelte';
-
-  const { sampleStore } = getContext('app');
-
-  $: samples = [
-    ...$sampleStore['STRUCTURED_SAMPLE'],
-    ...$sampleStore['RANDOM_SAMPLE']
-  ];
-
-  console.log(samples);
 </script>
