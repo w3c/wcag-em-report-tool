fa0ec75ae417ef33576def2f8451b62b961ee375
Roel Antonisse <r.antonisse@accessibility.nl>
2020-11-25 17:12:11 +0100

    Update EvaluationLayout;
    Initialize the first subject (type WebSite)

--- src/components/layouts/EvaluationLayout.svelte
+++ src/components/layouts/EvaluationLayout.svelte
@@ -1,3 +1,22 @@
 <!-- @Layout:Evaluation -->
 <slot />
 <!-- /@Layout -->
+
+<script>
+  import { getContext } from 'svelte';
+  import { subject } from '../../data/stores/earl/subjectStore.js';
+
+  const { scopeStore } = getContext('app');
+  // Initialize
+
+  // Create a WebSite subject
+  let websiteSubject = subject({
+    ID: 1,
+    type: 'WebSite',
+  });
+
+  $: {
+    websiteSubject.title = $scopeStore['SITE_NAME'];
+    websiteSubject.description = $scopeStore['WEBSITE_SCOPE'];
+  }
+</script>
