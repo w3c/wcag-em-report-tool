bfea5ba5a62114d28b9814b7fbdb00e24c46b9e1
Roel Antonisse <r.antonisse@accessibility.nl>
2020-12-09 15:46:46 +0100

    Update evaluationStore, add all dependent data / stores

--- src/data/stores/evaluationStore.js
+++ src/data/stores/evaluationStore.js
@@ -1,4 +1,5 @@
 import { derived } from 'svelte/store';
+import { locale } from 'svelte-i18n';
 
 // Import related stores and combine
 import scopeStore from './scopeStore.js';
@@ -6,49 +7,74 @@ import exploreStore from './exploreStore.js';
 import sampleStore from './sampleStore.js';
 import summaryStore from './summaryStore.js';
 
-export default derived([
-  scopeStore,
-  exploreStore,
-  sampleStore,
-  summaryStore
-], (
-  $scopeStore,
-  $exploreStore,
-  $sampleStore,
-  $summaryStore
-) => {
-  return {
-    language: '',
-    defineScope: {
-      scope: {
-        // TestSubject with type WebSite
-      },
+import assertionStore from './earl/assertionStore.js';
+import { subject } from './earl/subjectStore.js';
 
-      // https://www.w3.org/WAI/standards-guidelines/wcag/#versions
-      wcagVersion: '',
-      conformanceTarget: '',
+export default derived(
+  [
+    assertionStore,
+    locale,
+    subject,
+    scopeStore,
+    exploreStore,
+    sampleStore,
+    summaryStore
+  ],
+  ([
+    $assertionStore,
+    $locale,
+    $subject,
+    $scopeStore,
+    $exploreStore,
+    $sampleStore,
+    $summaryStore
+  ]) => {
+    const { CONFORMANCE_TARGET, WCAG_VERSION } = $scopeStore;
+    const { RANDOM_SAMPLE, STRUCTURED_SAMPLE } = $sampleStore;
 
-    },
-    exploreTarget: {
-      technologiesReliedUpon: [],
-      essentialFunctionality: '',
-      pageTypeVariety: ''
-    },
-    selectSample: {
-      structuredSample: [],
-      randomSample: []
-    },
-    auditSample: [
-      // earl:Assertion collection
-    ],
-    report: {
-      evaluator: {
-        // Agent / earl:MainAssertor
+    const {
+      EVALUATION_CREATOR,
+      EVALUATION_COMMISSIONER,
+      EVALUATION_DATE,
+      EVALUATION_SPECIFICS,
+      EVALUATION_SUMMARY,
+      EVALUATION_TITLE
+    } = $summaryStore;
+
+    const {
+      ESSENTIAL_FUNCTIONALITY,
+      PAGE_TYPES,
+      TECHNOLOGIES_RELIED_UPON
+    } = $exploreStore;
+
+    return {
+      language: $locale,
+      defineScope: {
+        // First subject === scope / website
+        scope: $subject(1),
+
+        // https://www.w3.org/WAI/standards-guidelines/wcag/#versions
+        wcagVersion: WCAG_VERSION,
+        conformanceTarget: CONFORMANCE_TARGET
       },
-      commissioner: {
-        // Agent / earl:Assertor?
+      exploreTarget: {
+        technologiesReliedUpon: TECHNOLOGIES_RELIED_UPON,
+        essentialFunctionality: ESSENTIAL_FUNCTIONALITY,
+        pageTypeVariety: PAGE_TYPES
       },
-      date: ''
-    }
-  };
-});
+      selectSample: {
+        randomSample: RANDOM_SAMPLE,
+        structuredSample: STRUCTURED_SAMPLE
+      },
+      auditSample: $assertionStore,
+      report: {
+        commissioner: EVALUATION_COMMISSIONER,
+        date: EVALUATION_DATE,
+        evaluator: EVALUATION_CREATOR,
+        specifics: EVALUATION_SPECIFICS,
+        summary: EVALUATION_SUMMARY,
+        title: EVALUATION_TITLE
+      }
+    };
+  }
+);
