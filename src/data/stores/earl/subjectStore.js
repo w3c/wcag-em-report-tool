d4479b6c0ebc6a57d74bc1af4a99303760e680a9
Roel Antonisse <r.antonisse@accessibility.nl>
2020-12-24 10:44:25 +0100

    Update subjectStore; add initialSubjectStore

--- src/data/stores/earl/subjectStore.js
+++ src/data/stores/earl/subjectStore.js
@@ -2,6 +2,15 @@ import collectionStore from '../collectionStore.js';
 
 import { TestSubject } from './models.js';
 
-const $subjects = collectionStore(TestSubject, []);
+export const initialSubjectStore = [
+  new TestSubject({
+    ID: 1,
+    type: 'WebSite',
+    title: '',
+    description: ''
+  })
+];
+
+const $subjects = collectionStore(TestSubject, initialSubjectStore);
 
 export default $subjects;
