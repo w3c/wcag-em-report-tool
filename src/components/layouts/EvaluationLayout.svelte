54a8869fb6a4f225c22dbacbd62d7c7c8bf1c843
Roel Antonisse <r.antonisse@accessibility.nl>
2020-12-27 22:45:32 +0100

    Update EvaluationLayout; add background subscription to stores
    (teststore and outcomes)

--- src/components/layouts/EvaluationLayout.svelte
+++ src/components/layouts/EvaluationLayout.svelte
@@ -3,7 +3,7 @@
 <!-- /@Layout -->
 
 <script>
-  import { getContext, setContext } from 'svelte';
+  import { getContext, onDestroy, onMount, setContext } from 'svelte';
   import { outcomeValueStore } from '../../data/stores/earl/resultStore.js';
   import subjects from '../../data/stores/earl/subjectStore.js';
   import testStore from '../../data/stores/earl/testStore.js';
@@ -12,11 +12,15 @@
   // Initialize
 
   // Create a WebSite subject
-  let websiteSubject = $subjects.find((subject) => {
-    return subject.type.indexOf('WebSite') >= 0;
-  }) || subjects.create({
-    type: 'WebSite'
-  });
+  let websiteSubject =
+    $subjects.find((subject) => {
+      return subject.type.indexOf('WebSite') >= 0;
+    }) ||
+    subjects.create({
+      type: 'WebSite'
+    });
+
+  let endSubscription;
 
   $: {
     websiteSubject.title = $scopeStore['SITE_NAME'];
@@ -26,5 +30,22 @@
   setContext('Evaluation', {
     outcomeValues: outcomeValueStore,
     testCriteria: testStore
+  });
+
+  onMount(() => {
+    // Stores that need to be up-to-date in background
+    endSubscription = (() => {
+      const unscribeOutcomeStore = outcomeValueStore.subscribe(() => {});
+      const unscribeTestStore = testStore.subscribe(() => {});
+
+      return () => {
+        unscribeOutcomeStore();
+        unscribeTestStore();
+      };
+    })();
+  });
+
+  onDestroy(() => {
+    endSubscription();
   });
 </script>
