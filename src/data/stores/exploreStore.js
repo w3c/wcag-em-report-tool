c756376be41f9eade0ba450963e5c1d16fa9fb23
Roel Antonisse <r.antonisse@accessibility.nl>
2021-01-13 16:35:18 +0100

    Update exploreStore; Add writable webTechnologyStore

--- src/data/stores/exploreStore.js
+++ src/data/stores/exploreStore.js
@@ -1,10 +1,14 @@
 import { writable } from 'svelte/store';
 
+import webTechnologies from '../webtechnologies.json';
+
 export const initialExploreStore = {
   TECHNOLOGIES_RELIED_UPON: [],
   ESSENTIAL_FUNCTIONALITY: '',
   PAGE_TYPES: ''
 };
+
+export const webTechnologyStore = writable([...webTechnologies]);
 
 const exploreStore = writable({...initialExploreStore});
 
