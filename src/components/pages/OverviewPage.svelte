67b2ffb0141887a9677c7ccefa5d04c330e7b901
Roel Antonisse <r.antonisse@accessibility.nl>
2021-01-18 10:19:31 +0100

    Update OverviewPage; Change translation constants into one TRANSLATED

--- src/components/pages/OverviewPage.svelte
+++ src/components/pages/OverviewPage.svelte
@@ -2,28 +2,28 @@
  * @component
  *   OverviewPage
  * -->
-<Page title="{PAGE_TITLE}">
+<Page title="{TRANSLATED.PAGE_TITLE}">
   <p>
-    {@html INTRODUCTION_P1}
+    {@html TRANSLATED.INTRODUCTION_P1}
   </p>
   <p>
-    {@html INTRODUCTION_P2}
+    {@html TRANSLATED.INTRODUCTION_P2}
   </p>
 
-  <Details label="<h2>{USAGE_HEADING}</h2>">
+  <Details label="<h2>{TRANSLATED.USAGE_HEADING}</h2>">
     <ul>
-      <li>{USAGE_LI1}</li>
-      <li>{USAGE_LI2}</li>
-      <li>{USAGE_LI3}</li>
+      <li>{TRANSLATED.USAGE_LI1}</li>
+      <li>{TRANSLATED.USAGE_LI2}</li>
+      <li>{TRANSLATED.USAGE_LI3}</li>
     </ul>
   </Details>
 
-  <Details label="<h2>{TIPS_HEADING}</h2>">
+  <Details label="<h2>{TRANSLATED.TIPS_HEADING}</h2>">
     <ol>
-      <li>{TIPS_LI1}</li>
-      <li>{TIPS_LI2}</li>
-      <li>{TIPS_LI3}</li>
-      <li>{TIPS_LI4}</li>
+      <li>{TRANSLATED.TIPS_LI1}</li>
+      <li>{TRANSLATED.TIPS_LI2}</li>
+      <li>{TRANSLATED.TIPS_LI3}</li>
+      <li>{TRANSLATED.TIPS_LI4}</li>
     </ol>
   </Details>
 </Page>
@@ -36,18 +36,21 @@
   import Page from '../Page.svelte';
 
   const { translate } = getContext('app');
-  $: PAGE_TITLE = $translate('PAGES.START.SUBTITLE');
-  $: INTRODUCTION_P1 = $translate('PAGES.START.INTRO_1', { values: links });
-  $: INTRODUCTION_P2 = $translate('PAGES.START.INTRO_2');
-  $: USAGE_HEADING = $translate('PAGES.START.USAGE_HD');
-  $: USAGE_LI1 = $translate('PAGES.START.USAGE_LI1');
-  $: USAGE_LI2 = $translate('PAGES.START.USAGE_LI2');
-  $: USAGE_LI3 = $translate('PAGES.START.USAGE_LI3');
-  $: TIPS_HEADING = $translate('PAGES.START.TIPS_HD');
-  $: TIPS_LI1 = $translate('PAGES.START.TIPS_LI1');
-  $: TIPS_LI2 = $translate('PAGES.START.TIPS_LI2');
-  $: TIPS_LI3 = $translate('PAGES.START.TIPS_LI3');
-  $: TIPS_LI4 = $translate('PAGES.START.TIPS_LI4');
+
+  $: TRANSLATED = {
+    PAGE_TITLE: $translate('PAGES.START.SUBTITLE'),
+    INTRODUCTION_P1: $translate('PAGES.START.INTRO_1', { values: links }),
+    INTRODUCTION_P2: $translate('PAGES.START.INTRO_2'),
+    USAGE_HEADING: $translate('PAGES.START.USAGE_HD'),
+    USAGE_LI1: $translate('PAGES.START.USAGE_LI1'),
+    USAGE_LI2: $translate('PAGES.START.USAGE_LI2'),
+    USAGE_LI3: $translate('PAGES.START.USAGE_LI3'),
+    TIPS_HEADING: $translate('PAGES.START.TIPS_HD'),
+    TIPS_LI1: $translate('PAGES.START.TIPS_LI1'),
+    TIPS_LI2: $translate('PAGES.START.TIPS_LI2'),
+    TIPS_LI3: $translate('PAGES.START.TIPS_LI3'),
+    TIPS_LI4: $translate('PAGES.START.TIPS_LI4')
+  };
 
   // @todo: Add these links the locales
   // (They might be used on another page)
