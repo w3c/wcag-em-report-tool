fbd2fb2455fad4f0cab562e8ad13809b1cf299af
Roel Antonisse <r.antonisse@accessibility.nl>
2021-01-22 15:18:35 +0100

    Update OverviewPage translations; Change to markdown instead of variables

--- src/components/pages/OverviewPage.svelte
+++ src/components/pages/OverviewPage.svelte
@@ -39,7 +39,7 @@
 
   $: TRANSLATED = {
     PAGE_TITLE: $translate('PAGES.START.SUBTITLE'),
-    INTRODUCTION_P1: $translate('PAGES.START.INTRO_1', { values: links }),
+    INTRODUCTION_P1: $translate('PAGES.START.INTRO_1'),
     INTRODUCTION_P2: $translate('PAGES.START.INTRO_2'),
     USAGE_HEADING: $translate('PAGES.START.USAGE_HD'),
     USAGE_LI1: $translate('PAGES.START.USAGE_LI1'),
@@ -50,19 +50,5 @@
     TIPS_LI2: $translate('PAGES.START.TIPS_LI2'),
     TIPS_LI3: $translate('PAGES.START.TIPS_LI3'),
     TIPS_LI4: $translate('PAGES.START.TIPS_LI4')
-  };
-
-  // @todo: Add these links the locales
-  // (They might be used on another page)
-  // or... use markdown inside translations...
-  $: links = {
-    link_wcag:
-      '<a href="http://www.w3.org/WAI/standards-guidelines/wcag/">Web Content Accessibility Guidelines (WCAG) 2</a>',
-    link_wcagem:
-      '<a href="http://www.w3.org/TR/WCAG-EM/">Web Content Accessibility Guidelines Evaluation Methodology</a>',
-    link_wcagem_overview:
-      '<a href="http://www.w3.org/WAI/test-evaluate/conformance/wcag-em/">WCAG-EM Overview</a>',
-    link_report_example:
-      '<a href="/evaluation/report/example/">structured report (example)</a>'
   };
 </script>
