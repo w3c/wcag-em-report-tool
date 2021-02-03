2df22e626a48bcfb491c2c765d4fe06b15894dd6
Roel Antonisse <r.antonisse@accessibility.nl>
2021-02-03 23:12:33 +0100

    Update i18n; Add internationalize to call first before loading app

--- src/data/i18n.js
+++ src/data/i18n.js
@@ -2,18 +2,19 @@ import { register, init } from 'svelte-i18n';
 
 import app from './app.js';
 
-// Asynchronous loading
-register('en', () => import('locales/translations_en.json'));
-register('nl', () => import('locales/translations_nl.json'));
+export async function internationalize() {
+  // Asynchronous loading
+  register('en', () => import('locales/translations_en.json'));
+  register('nl', () => import('locales/translations_nl.json'));
 
-init({
-  fallbackLocale: app.defaultLocale.lang,
-  // language[-region]
-  // e.g. /nl-NL/ --> Dutch Netherlands
-  // See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes for language codes
-  // See https://en.wikipedia.org/wiki/ISO_3166-1 for region codes
-  // initialLocale: getLocaleFromPathname(/^\/([a-z]{2})(\-[A-Z]{2})?\//)
-  initialLocale: 'en'
+  init({
+    fallbackLocale: app.defaultLocale.lang,
+    // language[-region]
+    // e.g. /nl-NL/ --> Dutch Netherlands
+    // See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes for language codes
+    // See https://en.wikipedia.org/wiki/ISO_3166-1 for region codes
+    // initialLocale: getLocaleFromPathname(/^\/([a-z]{2})(\-[A-Z]{2})?\//)
+    initialLocale: 'en'
 
-});
-// starts loading 'en'
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import App from 'components/App.svelte';
-import 'data/i18n.js';
+import { internationalize } from 'data/i18n.js';
 
-/**
- * See for options:
- * https://svelte.dev/docs#Creating_a_component
- */
-new App({
-  target: document.getElementById('wai-wcag-em-report-tool'),
-  props: {
-    // Replaced string, see rollup.config.json replace plugin.
-    basepath: '__BASEPATH__'
-  }
+internationalize().then(() => {
+  /**
+   * See for options:
+   * https://svelte.dev/docs#Creating_a_component
+   */
+  new App({
+    target: document.getElementById('wai-wcag-em-report-tool'),
+    props: {
+      // Replaced string, see rollup.config.json replace plugin.
+      basepath: '__BASEPATH__'
+    }
+  });
 });
