fabe9c0bee1caa912518022e5ea7d7e9944834a3
Hidde de Vries <hidde@hiddedevries.nl>
2021-02-11 14:50:59 +0100

    Move buttons out of box and make both primary
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/pages/Evaluation/ReportPage.svelte
+++ src/components/pages/Evaluation/ReportPage.svelte
@@ -3,19 +3,13 @@
     {@html TRANSLATED.INTRODUCTION}
   </p>
 
-  <aside class="box">
-    <header class="box-h">
-      <h2>{TRANSLATED.DOWNLOAD_REPORT_HEADING}</h2>
-    </header>
-    <div class="box-i">
-      <Button on:click="{handleHTMLDownloadClick}">
-        <span>{TRANSLATED.BUTTON_SAVE_HTML}</span>
-      </Button>
-      <Button type="secondary" on:click="{handleJSONDownloadClick}">
-        <span>{TRANSLATED.BUTTON_SAVE_JSON}</span>
-      </Button>
-    </div>
-  </aside>
+
+    <Button on:click="{handleHTMLDownloadClick}">
+      <span>{TRANSLATED.BUTTON_SAVE_HTML}</span>
+    </Button>
+    <Button on:click="{handleJSONDownloadClick}">
+      <span>{TRANSLATED.BUTTON_SAVE_JSON}</span>
+    </Button>
 
   <Report />
 </Page>
@@ -34,7 +28,6 @@
   $: TRANSLATED = {
     PAGE_TITLE: $translate('PAGES.REPORT.TITLE'),
     INTRODUCTION: $translate('PAGES.REPORT.INTRO'),
-    DOWNLOAD_REPORT_HEADING: $translate('PAGES.REPORT.DOWNLOAD_REPORT'),
     BUTTON_SAVE_HTML: $translate('PAGES.REPORT.BTN_SAVE_HTML'),
     BUTTON_SAVE_JSON: $translate('PAGES.REPORT.BTN_SAVE_JSON')
   };
