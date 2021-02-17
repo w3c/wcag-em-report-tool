4a58996283d8a45fa3e70beeb2dfd6e3d54e3556
Roel Antonisse <r.antonisse@accessibility.nl>
2021-02-17 17:17:33 +0100

    Update jsonld; Add getItems to extract from @graph or put the object in an array

--- src/scripts/jsonld.js
+++ src/scripts/jsonld.js
@@ -1,10 +1,15 @@
 import jsonld from 'jsonld/lib/jsonld.js';
 
+function getItems(ldon) {
+  return ldon['@graph'] || [ldon];
+}
+
 export default {
   compact: jsonld.compact,
   expand: jsonld.expand,
   flatten: jsonld.flatten,
   frame: jsonld.frame,
+  getItems,
   parseValue: (property) => property,
   sanitizeContext: (data) => data
 };
