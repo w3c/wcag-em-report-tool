cb68d500aa5876afd1b0309a09c3596f53671f0b
Hidde de Vries <hidde@hiddedevries.nl>
2021-02-17 11:16:09 +0100

    Add ResourceLink to Audit page, split note and intro
    Splitting note and intro addresses a potential 1.3.1 violation (using br instead of new paragraph), and makes more sence when we use the resource link pattern
    
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/pages/Evaluation/AuditPage.svelte
+++ src/components/pages/Evaluation/AuditPage.svelte
@@ -1,5 +1,7 @@
 <Page title="{TRANSLATED.PAGE_TITLE}">
   <p>{@html TRANSLATED.INTRODUCTION}</p>
+  <ResourceLink href="https://www.w3.org/TR/WCAG-EM/#step4">{TRANSLATED.RESOURCE_LINK_NAME}</ResourceLink>
+  <p>{@html TRANSLATED.INTRODUCTION_NOTE}</p>
   <AuditorImport />
   <Auditor />
 </Page>
@@ -9,13 +11,16 @@
 
   import Auditor from '@app/components/ui/Auditor/Auditor.svelte';
   import AuditorImport from '@app/components/ui/Auditor/AuditorImport.svelte';
+  import ResourceLink from '@app/components/ui/ResourceLink.svelte';
   import Page from '@app/components/ui/Page.svelte';
 
   const { translate } = getContext('app');
 
   $: TRANSLATED = {
     PAGE_TITLE: $translate('PAGES.AUDIT.TITLE'),
-    INTRODUCTION: $translate('PAGES.AUDIT.INTRO')
+    INTRODUCTION: $translate('PAGES.AUDIT.INTRO'),
+    INTRODUCTION_NOTE: $translate('PAGES.AUDIT.INTRO_NOTE'),
+    RESOURCE_LINK_NAME: $translate('PAGES.AUDIT.RESOURCE_LINK_NAME')
   };
 
 </script>
