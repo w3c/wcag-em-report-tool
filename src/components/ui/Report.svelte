ac306a8c2e7ff0778687d5dadd4d8aa00ce81ac1
Roel Antonisse <r.antonisse@accessibility.nl>
2021-02-17 13:47:42 +0100

    Update Report; replace $tests with $wcag

--- src/components/ui/Report.svelte
+++ src/components/ui/Report.svelte
@@ -46,7 +46,7 @@
 <p>{report.result || TRANSLATED.LABEL_NOT_PROVIDED}</p>
 
 <h2>{TRANSLATED.HEADING_AUDIT_RESULTS_DETAIL}</h2>
-<AuditorSummary criteria="{$tests}" />
+<AuditorSummary criteria="{$wcag}" />
 
 <h2>{TRANSLATED.HEADING_SAMPLE}</h2>
 {#if report.samples.length > 0}
