27488424f686aec80a90c73eae70c13e47a8e5b7
Roel Antonisse <r.antonisse@accessibility.nl>
2021-02-25 22:17:53 +0100

    Update scripts/jsonld; Add hasType, setIdFromProperties helpers

--- src/scripts/jsonld.js
+++ src/scripts/jsonld.js
@@ -1,7 +1,57 @@
 import jsonld from 'jsonld/lib/jsonld.js';
 
 function getItems(ldon) {
+  if (!ldon) {
+    return [];
+  }
+
   return ldon['@graph'] || [ldon];
+}
+
+function getType(item) {
+  let { type } = item;
+
+  if (!Array.isArray(type)) {
+    type = [type];
+  }
+
+  return type;
+}
+
+function hasType(item, types) {
+  const itemType = getType(item);
+
+  if (!itemType) {
+    return false;
+  }
+
+  if (!Array.isArray(types)) {
+    types = [types];
+  }
+
+  return types.some((type) => {
+    return itemType.indexOf(type) >= 0;
+  });
+}
+
+function setIdFromProperties(item, properties) {
+  const newItem = {...item};
+  const url = properties.reduce((href, property) => {
+    const value = newItem[property];
+    if (href) {
+      return href;
+    }
+
+    const newURL = getURL(value);
+
+    return newURL ? newURL.href : null;
+  }, null);
+
+  if (url) {
+    newItem.id = url;
+  }
+
+  return newItem;
 }
 
 export default {
@@ -12,4 +62,6 @@ export default {
   getItems,
   parseValue: (property) => property,
   sanitizeContext: (data) => data
+  hasType,
+  setIdFromProperties
 };
