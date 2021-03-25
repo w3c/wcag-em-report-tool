e551c5b5403ac0fe2ec207c7a768480dfc86273e
Hidde de Vries <hidde@hiddedevries.nl>
2021-03-25 12:26:13 +0100

    Fix open/close button position in closed state
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/ui/Panel.svelte
+++ src/components/ui/Panel.svelte
@@ -25,13 +25,20 @@
       on:click={toggle}
       aria-expanded="{open}"
     >
-      {@html TRANSLATED.SHOW_HIDE}
       <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <polyline points="15 18 9 12 15 6"></polyline>
-    </svg>
+      </svg>
+      {@html TRANSLATED.SHOW_HIDE}
     </button>
   {/if}
 </aside>
+
+<style>
+.your-report__showhide[aria-expanded="false"] svg {
+  margin-right: .25em;
+  margin-left: 0;
+}
+</style>
 
 <script>
   import { getContext } from 'svelte';
