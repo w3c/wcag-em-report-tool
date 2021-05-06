297a3b8952f334c32f4436293314404f46cd5c30
Hidde de Vries <hidde@hiddedevries.nl>
2021-05-06 11:13:36 +0200

    Use existing value
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/components/ui/Panel.svelte
+++ src/components/ui/Panel.svelte
@@ -14,7 +14,7 @@
       type="button"
       class="button-secondary button-small your-report__showhide"
       on:click={toggle}
-      aria-expanded="{open}"
+      aria-expanded="{$yourReportPanelOpen}"
     >
       {@html TRANSLATED.SHOW_HIDE}
       <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -30,7 +30,7 @@
       type="button"
       class="button-secondary button-small your-report__showhide"
       on:click={toggle}
-      aria-expanded="{open}"
+      aria-expanded="{$yourReportPanelOpen}"
     >
       <svg aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <polyline points="15 18 9 12 15 6"></polyline>
