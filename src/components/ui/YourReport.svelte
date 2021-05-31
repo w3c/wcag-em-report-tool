03f6ce71b6e5da4d5b6b7d307669242e19e4824f
Michel Hansma <michel.hansma@han.nl>
2021-05-31 10:38:32 +0200

    #460 Fixed warning message clear on confirm

--- src/components/ui/YourReport.svelte
+++ src/components/ui/YourReport.svelte
@@ -153,11 +153,14 @@
   }
 
   function handleClearEvaluationClick() {
-    window.confirm(TRANSLATED.CLEAR_WARNING, function() {
+    var clearResult = window.confirm(TRANSLATED.CLEAR_WARNING);
+    if(clearResult){
       $evaluationStore.reset();
       navigate($routes.SCOPE.path, { replace: true });
-    });
+    }
   }
+
+
 
   $: isOverview = $location.pathname === $routes.OVERVIEW.path; 
   $: isAuditSample = $location.pathname === $routes.AUDIT.path; 
