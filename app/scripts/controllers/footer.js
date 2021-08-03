b73290b287507fb4e6103ff89eb933f9085fe434
Hidde de Vries <hidde@hiddedevries.nl>
2021-08-03 15:59:25 +0200

    Place latest dist in GH pages
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- app/scripts/controllers/footer.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('wcagReporter')
-  .controller(
-    'FooterCtrl',
-    function ($scope, pkgData) {
-      $scope.pkg = pkgData;
-    }
-  );
--- app/scripts/templates.js
+++
@@ -1,5 +0,0 @@
-/**
- * This is a dummy script. When the app is build the module in this script
- * is replaced with a auto-generated compilation of all the templates.
- */
-angular.module('wert-templates', []);
